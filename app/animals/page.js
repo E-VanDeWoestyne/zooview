0cfb658c50c5b26571d59c3a139d17bca57d7f23
prec12 <chidinmaer1@gmail.com>
2025-04-10 16:29:58 -0600

    Your commit message describing your changes

--- app/animals/page.js
+++ app/animals/page.js
@@ -6,6 +6,14 @@ const AnimalsPage = () => {
       <div className="p-8 text-2xl text-green-800 text-center">
         animal list page
       </div>
+       
+        <div className="flex "> //justify-center items-center 
+          <div className="bg-gray-100 p-4 rounded-md shadow-md">
+          <img src="./img/images.jpeg" alt="animal" width="300" height={200}   style={{ borderRadius: '25px', display: 'block' }}
+               />  
+               <p className="=mt-2 text-center text-gray-700">hello evryone this is an ostrache </p>   
+          </div>
+        </div>
     </div>
   );
 };
--- app/tickets/page.js
+++ app/tickets/page.js
@@ -5,6 +5,14 @@ const TicketsPage = () => {
     <div className="min-h-screen bg-green-100">
       <div className="p-8 text-2xl text-green-800 text-center">
         tickets page
+        
+        <div className="bg-white shadow-md rounded-lg p-4 mt-4">
+          <p className="text-xl text-green-700 text-center">
+            Child(3-15)
+            </p>
+            <div className="w-200px h-200px border-solid-5px border-radius: 50%;">-</div>
+           
+        </div>
       </div>
     </div>
   );
